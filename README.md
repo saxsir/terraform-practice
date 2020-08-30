a6b04f3637a34e6255d51e0df5453887e78d8e00
saxsir <saxsir.256+github@gmail.com>
2020-08-30 23:34:36 +0900

    練習おわり

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 terraform-practice
 ===
+
+## 3章
+
+* resource
+* variable
+* locals
+    * ローカル変数はコマンド実行時に上書きできない
+        * 基本的にこっちの方が追いやすそう。必要があれば `variable` を使う
+    * モジュールから値を取る時とかは `variable`か
+* output
+* data: データソース
+* provider
+    * AWS以外も操作可能
+* module
+  * getが必要
+
+まずはEC2から入るのAWSの理解に良さそう。
+
--- example/main.tf
+++ example/main.tf
@@ -2,51 +2,12 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
+module "web_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
 }
 
-resource "aws_security_group" "example_ec2" {
-  name = "example-ec2"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "public_dns" {
+  value = module.web_server.public_dns
 }
 
-resource "aws_instance" "example" {
-  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.example_ec2.id]
-
-  tags = {
-    Name = "example"
-  }
-
-  user_data = file("./user_data.sh")
-}
-
-output "example_instance_id" {
-  value = aws_instance.example.public_dns
-}
