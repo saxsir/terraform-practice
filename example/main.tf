ab8085f0b94510353cbe4b4b4d2a97675fcd6acf
saxsir <saxsir.256+github@gmail.com>
2020-08-30 23:24:18 +0900

    http server

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,52 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  tags = {
+    Name = "example"
+  }
+
+  user_data = file("./user_data.sh")
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.public_dns
 }
